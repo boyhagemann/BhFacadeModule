56e1e093887be480255bfcb1947e82c294a63293
boyhagemann <boy@swis.nl>
2013-01-24 12:03:18 +0100

    update module paths

--- src/BhFacadeModule/Module.php
+++ src/BhFacadeModule/Module.php
@@ -39,7 +39,7 @@ class Module
     
     public function getConfig()
     {
-        return include __DIR__ . '/config/module.config.php';
+        return include __DIR__ . '/../../config/module.config.php';
     }
 
     public function getAutoloaderConfig()
@@ -47,7 +47,7 @@ class Module
         return array(
             'Zend\Loader\StandardAutoloader' => array(
                 'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                    __NAMESPACE__ => __DIR__ . '/../../src/' . __NAMESPACE__,
                 ),
             ),
         );
